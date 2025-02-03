aaf6207f9a045c0adad692f004ba77a191855239
Nadu C <charawe.yw@gmail.com>
2025-02-03 12:37:17 +0300

    determine if all boxes in a list of n can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the inner boxes inside 'boxes' can be opened
+    Args:
+        boxes (list of lists)
+    Returns: True or False
+    """
+    keys = sum(boxes, [])
+    unique_keys = sorted(set(sum(boxes, [])))
+    for i in range(1, len(boxes)):
+        alt_keys = sorted(set(sum(boxes[0:i] + boxes[i + 1:], [])))
+        if i not in unique_keys:
+            return False
+        if i not in alt_keys:
+            return False
+    return True
